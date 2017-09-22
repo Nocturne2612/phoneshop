dfe16c3dd0eec437622c001d0ecd518d042b285f
phamduytan <tanpd@peacesoft.net>
2017-09-22 17:29:22 +0700

    add frontend login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -10,13 +10,13 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="site-login" ">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    
     <p>Please fill out the following fields to login:</p>
-
+    
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-5" >
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -25,12 +25,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div style="color:#999;margin:1em 0">
+                <div style="color:#999; margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 </div>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-danger btn-md btn-login fwb', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
