ba95818e2c2fb8ddb906344d0a6e0da7be5be403
phamduytan <tanpd@peacesoft.net>
2017-10-10 08:56:26 +0700

    a

--- frontend/widgets/views/mainMenuWidget.php
+++ frontend/widgets/views/mainMenuWidget.php
@@ -11,6 +11,9 @@
     $catNew = $new->getNewsBy();
     // $catNewNull = $new->getNews();
 
+    // echo "<prE>";
+    // print_r($catNewNull);
+    // die;
 ?>
 <div class="col-md-9">
     <ul class="menu clearfix visible-lg visible-md" ">
@@ -45,30 +48,19 @@
             foreach ($catNew  as $value) {
         ?>
         <li>
-<<<<<<< HEAD
-            <a href="<?=Yii::$app->homeUrl?>news/<?php echo $value["newsCatId"] ?>"><?php echo $value["newsCatName"] ?></a>
-            <?php  
-                if($catNews){
-=======
             <a href="<?=Yii::$app->homeUrl?>news/listnew?id=<?php echo $value["newsCatId"] ?>"><?php echo $value["newsCatName"] ?></a>
             <?php  
                 $catSubNew = $new->getNewsBy($value["newsCatId"]);
                
                 if($catSubNew){
->>>>>>> 86c4460a5efebbf0a2f9fc401b3c5f0d95d40405
             ?>
                 <ul class="sub-menu">
                     <?php  
-                        foreach ($catNews as $val) {
+                        foreach ($catNew as $val) {
                     ?>
-<<<<<<< HEAD
-                        <li><a href="<?=Yii::$app->homeUrl?>news/<?php echo $val["newsCatId"] ?>"><?php echo $val["newsCatName"] ?></a></li>
-=======
                         <li><a href="<?=Yii::$app->homeUrl?>news/listnew?id=<?php echo $val["newsCatId"] ?>"><?php echo $val["newsCatName"] ?></a></li>
->>>>>>> 86c4460a5efebbf0a2f9fc401b3c5f0d95d40405
-                    <?php } ?> 
+                    <?php } ?>
                 </ul>
-
             <?php } ?>
 
         </li>
